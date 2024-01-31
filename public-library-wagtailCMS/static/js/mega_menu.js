5b566f4932dccbc0785cb208212607dd39cea8dc
danlerche <dan.lerch@gmail.com>
2024-01-31 15:40:25 -0800

    potential fix for expandable items are expanded on keyboard focus

--- public-library-wagtailCMS/static/js/mega_menu.js
+++ public-library-wagtailCMS/static/js/mega_menu.js
@@ -1,58 +1,39 @@
-$("#mainMenu .dropdown-menu").hover(function(){
-  $(this).addClass("show");
-  $(this).prev().addClass("show");
-  $(this).prev().attr("aria-expanded", "true");
-  $(this).prev().parent().addClass("menu-border");
-
-  },
-  function(){
-  $(this).removeClass("show");
-  $(this).prev().removeClass("show");
-  $(this).prev().parent().removeClass("menu-border");
-}
-);
-$("#mainMenu a.dropdown-toggle").click(function(event){
-  $(this).removeClass("show");
-  $("#mainMenu .dropdown-menu").removeClass("show");
+//allows toggling menus open with the enter key
+$("#mainMenu a.dropdown-toggle").click(function(event) {
+  if ($(this).hasClass("show")) {
+    // If the class 'show' is already present, remove it
+    $(this).removeClass("show");
+    $(this).next().removeClass("show");
+    $(this).parent().removeClass("menu-border");
+    $(this).attr("aria-expanded", "false");
+  } else {
+    // If the class 'show' is not present, add it
+    $(this).addClass("show");
+    $(this).next().addClass("show");
+    $(this).parent().addClass("menu-border");
+    $(this).attr("aria-expanded", "true");
+  }
 });
 
-$("#mainMenu a.dropdown-toggle").hover(function(){
-  $(this).addClass("show");
-  $(this).parent().addClass("menu-border");
-  $(this).next().addClass("show");
-  $("#mainMenu a.show").attr("aria-expanded", "true");
-  $(this).focus();
-}
-);
-$("#mainMenu a.dropdown-toggle").mouseout(function(){
-  $(this).removeClass("show");
-  $(this).parent().removeClass("menu-border");
-  $(this).next().removeClass("show");
-  $(this).attr("aria-expanded", "false");
-  $("#mainMenu a").blur();
-
-});
 $("#mainMenu a.dropdown-toggle").focusin(function(){
 $("#mainMenu .show").removeClass("show");
 $("#mainMenu li.menu-border").removeClass("menu-border");
 $("#mainMenu a.dropdown-toggle").attr("aria-expanded", "false");
-$(this).addClass("show");
-$(this).parent().addClass("menu-border");
-$(this).attr("aria-expanded", "true");
-$(this).next().addClass("show");
 });
-$("#searchBox #searchForm input").focusin(function(){
-$("#mainMenu .show").removeClass("show");
-$("#mainMenu li.menu-border").removeClass("menu-border");
-$("#mainMenu a.dropdown-toggle").attr("aria-expanded", "false");
-});
-$("#logoArea a.navbar-brand").focusin(function(){
-$("#mainMenu .show").removeClass("show");
-$("#mainMenu li.menu-border").removeClass("menu-border");
-$("#mainMenu a.dropdown-toggle").attr("aria-expanded", "false");
-});
-//jQuery detect user pressing enter
-$("#mainMenu a.dropdown-toggle").click(function(event){
+
+
+//Adds hover 
+$("#mainMenu a.dropdown-toggle").hover(function(){
   $(this).addClass("show");
   $(this).next().addClass("show");
-});
+  $(this).parent().addClass("menu-border");
+  $(this).attr("aria-expanded", "true");
+
+  },
+  function(){
+    $(this).removeClass("show");
+    $(this).next().removeClass("show");
+    $(this).parent().removeClass("menu-border");
+    $(this).attr("aria-expanded", "false");
+}
+);
